17ea4d8a032eee3ac55a063fecebfbb4474fe106
Mike <dummy@dummy.com>
2018-06-16 15:56:59 +0200

    Added command line parser
    The SimpleCommandLineParser and an interface were added.

---
+++ cs-utilities/Parser/CommandLine/ICommandLineParser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Parser.CommandLine
+{
+    interface ICommandLineParser
+    {
+        /// <summary>
+        ///     Converts the value of command line argument to the appropriate type
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type, the commands value should be converted to
+        /// </typeparam>
+        /// <param name="command">
+        ///     The 'command' whose 'value' should be accessed.
+        /// </param>
+        /// <returns>
+        ///     The command line value for the specified command line parameter converted to T. 
+        ///     If the command is not found or any exception occurs while trying to convert it
+        ///     default(T) is returned.
+        /// </returns>
+        T Get<T>(string command);
+
+        /// <summary>
+        ///     Sets the array of commands aquired from the command line, which will be parsed
+        /// </summary>
+        string[] Commands { set; }
+    }
+}
---
+++ cs-utilities/Parser/CommandLine/SimpleCommandLineParser.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Utilities.Parser.CommandLine
+{
+    public class SimpleCommandLineParser : ICommandLineParser
+    {
+        //The prefix of the commands
+        private const string COMMAND_PREFIX = "-";
+        //An array of the available commands
+        private static readonly string[] AVAILABLE_COMMANDS = {
+                                                                "command1",
+                                                                "command2",
+                                                                "command3"
+                                                            };
+        //The storage for the parsed commands; Key: argument name; Value: argument value
+        private readonly Dictionary<string, string> commands;
+
+        public SimpleCommandLineParser()
+        {
+            //Initialize the dictionary with a default size
+            this.commands = new Dictionary<string, string>();
+        }
+
+        public string[] Commands
+        {
+            set
+            {
+                //Clear any previous commands
+                this.commands.Clear();
+                //Parse the commands
+                if (value != null)
+                    this.ParseCommands(value);
+            }
+        }
+
+        public T Get<T>(string command)
+        {
+            try
+            {
+                //Make the key lower case and trim any whitespaces
+                command = command.ToLower().Trim();
+                //The default value of the type T, if the command is not present
+                if (!this.commands.ContainsKey(command))
+                    return default(T);
+                //The default value of the type T, if no command value was specified
+                if (string.IsNullOrWhiteSpace(this.commands[command]) || this.commands[command] == string.Empty)
+                    return default(T);
+                //Convert the string the the specified type
+                return (T)TypeDescriptor.GetConverter(typeof(T)).ConvertFromString(this.commands[command]);
+            }
+            catch (Exception)
+            {
+                //If any exception occurs we just return the default value of T
+                return default(T);
+            }
+        }
+
+        /// <summary>
+        ///     Does the actual parsing of the parameters
+        /// </summary>
+        /// <param name="arguments">
+        ///     The command line parameters. Expected format: -command1 value -command2 value
+        /// </param>
+        private void ParseCommands(string[] arguments)
+        {
+            //Create Command-Value-Pairs, filter out any disallowed command, put the commands in the dictionary
+            var argPairs = arguments.Where((arg, i) => i < arguments.Length - 1)
+                                    .Select((arg, i) => new { Command = arg.Replace(COMMAND_PREFIX, string.Empty).ToLower(), Value = arguments[i + 1].ToLower() })
+                                    .Where(pair => AVAILABLE_COMMANDS.Contains(pair.Command));
+            //Put the pairs in the dictionary
+            foreach (var pair in argPairs)
+                this.commands[pair.Command.Trim()] = pair.Value.Trim();
+        }
+    }
+}
